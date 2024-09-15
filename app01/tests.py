42575e9680819a0a09e8d4a07a35890c112e4210
juwenzhang <3137536916@qq.com>
2024-09-15 09:36:00 +0800

    feat:测试文件的搭建完毕

--- app01/tests.py
+++ app01/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+import os
+import django
+if __name__ == "__main__":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_bbs.settings')
+    django.setup()
+    # 测试文件的搭建
